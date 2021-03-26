b72f737ba0b9f7dc6f3829cd26cfc747b02c34f2
oopsmails <oopsmails@gmail.com>
2021-03-26 16:58:19 -0400

    20210326: adding org.testcontainers.kafka jar

--- spring-boot-kafka-microservice-email/src/test/java/com/oopsmails/kafka/ms/email/MessagingApiApplicationTests.java
+++ spring-boot-kafka-microservice-email/src/test/java/com/oopsmails/kafka/ms/email/MessagingApiApplicationTests.java
@@ -2,8 +2,12 @@ package com.oopsmails.kafka.ms.email;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
+@DirtiesContext
+@EmbeddedKafka(partitions = 1, brokerProperties = { "listeners=PLAINTEXT://localhost:9092", "port=9092" })
 class MessagingApiApplicationTests {
 
 	@Test
