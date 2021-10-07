226977ccb98ee30181a51920b6db83d228d8966d
oopsmails <oopsmails@gmail.com>
2021-10-07 14:28:50 -0400

    20211007: spring-boot-mock-backend, revised

---
+++ spring-boot-mock-backend/src/main/java/com/oopsmails/springboot/mockbackend/SpringBootBackendMockApplication.java
@@ -0,0 +1,55 @@
+package com.oopsmails.springboot.mockbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
+
+@SpringBootApplication //(scanBasePackages = "com.oopsmails.genericjava")
+@ComponentScan("com.oopsmails.genericjava")
+public class SpringBootBackendMockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackendMockApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS");
+            }
+        };
+    }
+
+    /**
+     * Not really using, just for demo example purpose here.
+     */
+    @RestController
+    class FileResource {
+
+        @GetMapping(path = "/api/files", produces = MediaType.TEXT_PLAIN_VALUE)
+        public ResponseEntity<String> getFile() {
+            String exportedContent = "Hello, World!";
+            String filename = "my-file.txt";
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccessControlExposeHeaders(Collections.singletonList("Content-Disposition"));
+            headers.set("Content-Disposition", "attachment; filename=" + filename);
+            return new ResponseEntity<>(exportedContent, headers, HttpStatus.OK);
+        }
+
+    }
+}
--- spring-boot-mock-backend/src/test/java/com/oopsmails/springboot/mockbackend/SpringBootBackendMockApplicationTest.java
+++ spring-boot-mock-backend/src/test/java/com/oopsmails/springboot/mockbackend/SpringBootBackendMockApplicationTest.java
@@ -1,30 +1,23 @@
-package com.oopsmails.genericjava.service;
+package com.oopsmails.springboot.mockbackend;
 
+import com.oopsmails.genericjava.service.MyService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest("service.message=Hello")
-public class MyServiceTest {
-
+@SpringBootTest("service.message=Hello in SpringBootBackendMockApplicationTest")
+public class SpringBootBackendMockApplicationTest {
     @Autowired
     private MyService myService;
 
     @Test
     public void contextLoads() {
-//		assertThat(myService.message()).isNotNull();
     }
 
     @Test
     public void serviceMessageNotNull() {
         assertThat(myService.message()).isNotNull();
     }
-
-    @SpringBootApplication
-    static class TestConfiguration {
-    }
-
 }
